da87481a9af857ffd37f6e4f3b780335662f2606
Gino Guerra <ginoiguerra@gmail.com>
2023-10-10 12:16:06 -0400

    messed around with string methods

---
+++ index.js
@@ -0,0 +1,10 @@
+let firstName = 'todd'
+let age = 25
+let str = 'hello JavaScripts students!'
+let str2 = '    Hello!    '
+
+const phrase = `Hello ${firstName}. You are ${age} today!`
+console.log(phrase.includes("You aa"))
+
+const num = "24" - '26'
+console.log(num)
